4ff83bb235241e306fe0def8f737fc7f300667b1
chlqudwnd <chio6622@gmail>
2019-09-17 13:20:40 +0900

    cpmlete

--- underbar-review/src/underbar.js
+++ underbar-review/src/underbar.js
@@ -77,6 +77,11 @@ _.uniq = function(array) {
 
 // Return the results of applying an iterator to each element.
 _.map = function(collection, iterator) {
+  let result = []
+  for(let i = 0; i < collection.length; i ++){
+    result = iterator(collection[i])
+  }
+  return result;
   // map() is a useful primitive iteration function that works a lot
   // like each(), but in addition to running the operation on all
   // the members, it also maintains an array of results.
